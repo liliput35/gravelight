7fb0108c8b63008f698e68f4b94efda5b5fa658e
Lorenz Ciocon <lorenzciocon@gmail.com>
2025-10-06 23:42:08 +0800

    init project; setup test movement

---
+++ Gravelight/Assets/Test/Scripts/PlayerController.cs
@@ -0,0 +1,110 @@
+using UnityEngine;
+using UnityEngine.Playables;
+
+
+[DefaultExecutionOrder(-1)]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private CharacterController _characterController;
+
+    [Header("Base Movement")]
+    public float runAcceleration = 0.25f;
+    public float runSpeed = 4f;
+    public float drag = 0.1f;
+    public float gravity = 25f;
+    public float jumpSpeed = 1f;
+    public float movingThreshold = 0.01f;
+    public float sprintAcceleration = 0.5f;
+    public float sprintSpeed = 7f;
+
+    private PlayerLocomotionInput _playerLocomotionInput;
+    private PlayerState _playerState;
+    private float _verticalVelocity = 0f;
+
+    private void Awake()
+    {
+        _playerLocomotionInput = GetComponent<PlayerLocomotionInput>();
+        _playerState = GetComponent<PlayerState>();
+    }
+
+    private void Update()
+    {
+        UpdateMovementState();
+        HandleVerticalMovement();
+        HandleLateralMovement();
+    }
+
+    private void UpdateMovementState()
+    {
+        bool isMovementInput = _playerLocomotionInput.MovementInput != Vector2.zero;
+        bool isMovingLaterally = IsMovingLaterally();
+        bool isSprinting = _playerLocomotionInput.SprintToggledOn && isMovingLaterally;
+        bool isGrounded = IsGrounded();
+
+        PlayerMovementState lateralState = isSprinting ? PlayerMovementState.Sprinting :
+                                            isMovingLaterally || isMovementInput ? PlayerMovementState.Running : PlayerMovementState.Idling;
+
+        _playerState.SetPlayerMovementState(lateralState);
+
+        if (!isGrounded && _characterController.velocity.y >= 0)
+        {
+            _playerState.SetPlayerMovementState(PlayerMovementState.Jumping);
+        }
+        else if (!isGrounded && _characterController.velocity.y < 0f)
+        {
+            _playerState.SetPlayerMovementState(PlayerMovementState.Falling);
+        }
+    }
+
+    private void HandleVerticalMovement()
+    {
+        bool isGrounded = _playerState.InGroundedState();
+
+        if (isGrounded && _verticalVelocity < 0)
+        {
+            _verticalVelocity = 0f;
+        }
+
+        _verticalVelocity -= gravity * Time.deltaTime;
+
+        if (_playerLocomotionInput.JumpPressed && isGrounded)
+        {
+            _verticalVelocity += Mathf.Sqrt(jumpSpeed * 3 * gravity);
+        }
+    }
+
+    private void HandleLateralMovement()
+    {
+        bool isSprinting = _playerState.CurrentPlayerMovementState == PlayerMovementState.Sprinting;
+        bool isGrounded = _playerState.InGroundedState();
+
+        float lateralAcceleration = isSprinting ? sprintAcceleration : runAcceleration;
+        float clampLateralMagnitude = isSprinting ? sprintSpeed : runSpeed;
+
+        // ? Use world axes (no camera influence)
+        Vector3 movementDirection =
+            new Vector3(_playerLocomotionInput.MovementInput.x, 0f, _playerLocomotionInput.MovementInput.y);
+
+        Vector3 movementDelta = movementDirection * lateralAcceleration * Time.deltaTime;
+        Vector3 newVelocity = _characterController.velocity + movementDelta;
+
+        Vector3 currentDrag = newVelocity.normalized * drag * Time.deltaTime;
+        newVelocity = (newVelocity.magnitude > drag * Time.deltaTime) ? newVelocity - currentDrag : Vector3.zero;
+        newVelocity = Vector3.ClampMagnitude(newVelocity, clampLateralMagnitude);
+        newVelocity.y += _verticalVelocity;
+
+        _characterController.Move(newVelocity * Time.deltaTime);
+    }
+
+    private bool IsMovingLaterally()
+    {
+        Vector3 lateralVelocity = new Vector3(_characterController.velocity.x, 0f, _characterController.velocity.z);
+        return lateralVelocity.magnitude > movingThreshold;
+    }
+
+    private bool IsGrounded()
+    {
+        return _characterController.isGrounded;
+    }
+}
+
---
+++ Gravelight/Assets/Test/Scripts/PlayerLocomotionInput.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[DefaultExecutionOrder(-2)]
+public class PlayerLocomotionInput : MonoBehaviour, InputSystem_Actions.IPlayerActions
+{
+    [SerializeField] private bool holdToSprint = true;
+
+    public bool SprintToggledOn { get; private set; }
+    public InputSystem_Actions InputActions { get; private set; }
+    public Vector2 MovementInput { get; private set; }
+    public bool JumpPressed { get; private set; }
+
+    private void OnEnable()
+    {
+        InputActions = new InputSystem_Actions();
+        InputActions.Enable();
+
+        InputActions.Player.Enable();
+        InputActions.Player.SetCallbacks(this);
+    }
+
+    private void OnDisable()
+    {
+        InputActions.Player.Disable();
+        InputActions.Player.RemoveCallbacks(this);
+    }
+
+    private void LateUpdate()
+    {
+        JumpPressed = false;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        MovementInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        // REMOVED - no longer using LookInput
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (!context.performed)
+            return;
+
+        JumpPressed = true;
+    }
+
+    public void OnToggleSprint(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            SprintToggledOn = holdToSprint || !SprintToggledOn;
+        }
+        else if (context.canceled)
+        {
+            SprintToggledOn = !holdToSprint && SprintToggledOn;
+        }
+    }
+}
