fed56d7c5461a9469e1b86af09916c2de16f7a9a
Lorenz Ciocon <lorenzciocon@gmail.com>
2025-10-27 10:34:31 +0800

    First Minutes flow set up

---
+++ Gravelight/Assets/Scripts/GrimDialogue.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using TMPro;
+
+public class GrimDialogue : MonoBehaviour, IInteractable
+{
+    [SerializeField] private string _prompt = "";
+    [SerializeField] private GameObject _promptUI;
+
+    [SerializeField] public Dialogue dialogue;
+    [SerializeField] private string[] firstDialogueLines;
+    [SerializeField] private string[] altDialogueLines;
+
+    private TMP_Text _promptText;
+    private GameFlowManager gameFlowManager; //  reference to the central manager
+    private bool hasTalkedAlready = false;
+
+    public string InteractionPrompt => _prompt;
+
+    private void Awake()
+    {
+        if (_promptUI != null)
+        {
+            _promptUI.SetActive(false);
+            _promptText = _promptUI.GetComponentInChildren<TMP_Text>();
+        }
+
+        // Try to auto-find GameFlowManager in the scene
+        gameFlowManager = FindFirstObjectByType<GameFlowManager>();
+    }
+
+    public bool Interact(Interactor interactor)
+    {
+        Debug.Log($"Talking with Grim: {gameObject.name}");
+
+        if (dialogue == null) return false;
+
+        if (!hasTalkedAlready)
+        {
+            // Start first-time dialogue
+            dialogue.StartDialogue("GRIM", firstDialogueLines);
+            hasTalkedAlready = true;
+
+            // Notify the GameFlowManager
+            gameFlowManager?.OnGrimFirstDialogueStarted(this);
+        }
+        else
+        {
+            // Alternate dialogue for later interactions
+            dialogue.StartDialogue("GRIM", altDialogueLines);
+        }
+
+        return true;
+    }
+
+    public void ShowPrompt()
+    {
+        if (_promptUI != null)
+        {
+            if (_promptText != null)
+                _promptText.text = _prompt;
+
+            _promptUI.SetActive(true);
+        }
+    }
+
+    public void HidePrompt()
+    {
+        if (_promptUI != null)
+            _promptUI.SetActive(false);
+    }
+}
